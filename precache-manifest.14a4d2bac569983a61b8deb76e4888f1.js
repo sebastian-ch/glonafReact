18b793f1eb2f837b02a6ca915172fcfd067b91dc
Sebastian Hancock <sebastian.hancock@tum.de>
2020-06-06 12:40:03 +0200

    Updates

---
+++ precache-manifest.14a4d2bac569983a61b8deb76e4888f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7cf5c3dd2b5c397e8ef9a1dd99540c",
+    "url": "/glonafReact/index.html"
+  },
+  {
+    "revision": "1418a578a17ff0a0e2ff",
+    "url": "/glonafReact/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "6c4d5b0326914391cd35",
+    "url": "/glonafReact/static/css/main.5bcb635e.chunk.css"
+  },
+  {
+    "revision": "1418a578a17ff0a0e2ff",
+    "url": "/glonafReact/static/js/2.093ad9eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glonafReact/static/js/2.093ad9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c4d5b0326914391cd35",
+    "url": "/glonafReact/static/js/main.06de45d8.chunk.js"
+  },
+  {
+    "revision": "caef032da7c76803ae39",
+    "url": "/glonafReact/static/js/runtime-main.a86c9360.js"
+  }
+]);
